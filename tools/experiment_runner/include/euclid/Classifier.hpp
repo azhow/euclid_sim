018ada5bbf16af73ab688ca63fd61f9ca1cb6f16
Arthur Zachow <arthur.zachow.dev@gmail.com>
2024-11-23 14:36:17 +0100

    feat: implemented EUCLID training

--- tools/experiment_runner/include/euclid/Classifier.hpp
+++ tools/experiment_runner/include/euclid/Classifier.hpp
@@ -1,10 +1,14 @@
 #ifndef EUCLID_CLASSIFIER_HPP
 #define EUCLID_CLASSIFIER_HPP
 
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
 
 #include "../experiment/IClassifier.hpp"
 #include "ExtendedCountSketch.hpp"
+#include "IPktFile.hpp"
+#include "experiment/Diagnoser.hpp"
 
 namespace Euclid {
 class Classifier : public Experiment::IClassifier {
@@ -12,15 +16,66 @@ public:
   Classifier(nlohmann::json classifier_params)
       : IClassifier(classifier_params),
         sensitivity_(classifier_params["sensitivity"]),
-        count_sketch_(classifier_params["count_sketch_depth"], classifier_params["count_sketch_width"]),
-        observation_window_(classifier_params["observation_window"]) {}
+        src_count_sketch_(classifier_params["count_sketch_depth"],
+                          classifier_params["count_sketch_width"]),
+        dst_count_sketch_(classifier_params["count_sketch_depth"],
+                          classifier_params["count_sketch_width"]),
+        observation_window_(classifier_params["observation_window"]),
+        ewma_src_baseline(0), ewma_dst_baseline(0), ewmmd_src_baseline(0),
+        ewmmd_dst_baseline(0), threshold_src(0), threshold_dst(0) {}
 
-  virtual void train(Pkt::Entry *entry) override {
-    // TODO
+  virtual void train(IPktFile &input) override {
+    const auto training_size{get_training_size(input.get_entry_count())};
+    for (size_t count = 1; count <= training_size; ++count) {
+      const Pkt::Entry *entry{input.read_next_entry()};
+
+      src_count_sketch_.update(entry->srcIp);
+      dst_count_sketch_.update(entry->dstIp);
+
+      // When the first OW is done we set the base values for the moving
+      // averages
+      if (count == observation_window_) {
+        ewma_src_baseline = src_count_sketch_.get_entropy();
+        ewma_dst_baseline = dst_count_sketch_.get_entropy();
+      }
+
+      if (count % observation_window_ == 0) {
+        // Reset ECS
+        src_count_sketch_.reset();
+        dst_count_sketch_.reset();
+
+        update_training_parameters();
+      }
+    }
+
+    // Update parameters one last time if needed - OW size is not divisor of training size
+    if (training_size % observation_window_ != 0) {
+        update_training_parameters();
+    }
+
+    std::cout << "============================================================="
+                 "=========\n";
+    std::cout << "Training done!\n";
+    std::cout << "Baseline parameters:\n";
+    std::cout << "\tSrc EWMA: " << ewma_src_baseline << "\n";
+    std::cout << "\tDst EWMA: " << ewma_dst_baseline << "\n";
+    std::cout << "\tSrc EWMMD: " << ewmmd_src_baseline << "\n";
+    std::cout << "\tDst EWMMD: " << ewmmd_dst_baseline << "\n";
+    std::cout << "\tSrc threshold: " << threshold_src << "\n";
+    std::cout << "\tDst threshold: " << threshold_dst << "\n";
+    std::cout << "============================================================="
+                 "========="
+              << std::endl;
   }
 
-  virtual void classify(Pkt::Entry *entry) override {
-    // TODO
+  virtual void classify(IPktFile &input,
+                        Experiment::Diagnoser &diagnoser) override {
+    // Reads from the end of the training until the end
+    for (const Pkt::Entry *entry = input.read_next_entry(); entry != nullptr;
+         entry = input.read_next_entry()) {
+      // classifier->classify(const_cast<Pkt::Entry *>(entry));
+      diagnoser.collect_stats(entry);
+    }
   }
 
   virtual uint64_t get_training_size(uint64_t dataset_size) const override {
@@ -31,15 +86,51 @@ public:
     std::cout << "\tClassifier: EUCLID" << std::endl;
     std::cout << "\tParams:" << std::endl;
     std::cout << "\t\tSensitivity: " << sensitivity_ << std::endl;
-    std::cout << "\t\tCount Sketch Depth: " << count_sketch_.get_depth() << std::endl;
-    std::cout << "\t\tCount Sketch Width: " << count_sketch_.get_width() << std::endl;
-    std::cout << "\t\tObservation Window Size: " << observation_window_ << std::endl;
+    std::cout << "\t\tCount Sketch Depth: "
+              << src_count_sketch_.get_depth() << std::endl;
+    std::cout << "\t\tCount Sketch Width: "
+              << src_count_sketch_.get_width() << std::endl;
+    std::cout << "\t\tObservation Window Size: " << observation_window_
+              << std::endl;
   }
 
 private:
-  const float sensitivity_;
-  ExtendedCountSketch count_sketch_;
+  const double sensitivity_;
+  ExtendedCountSketch src_count_sketch_;
+  ExtendedCountSketch dst_count_sketch_;
   const uint64_t observation_window_;
+
+  // Training calculated values
+  const double smoothing_{20 * 2e-8};
+  double ewma_src_baseline;
+  double ewma_dst_baseline;
+  double ewmmd_src_baseline;
+  double ewmmd_dst_baseline;
+  double threshold_src;
+  double threshold_dst;
+
+  double ewma_;
+  double ewmmd_;
+
+  void update_training_parameters() {
+      const auto src_entropy{src_count_sketch_.get_entropy()};
+      const auto dst_entropy{dst_count_sketch_.get_entropy()};
+
+      ewma_src_baseline =
+          smoothing_ * src_entropy + (1 - smoothing_) * ewma_src_baseline;
+      ewma_dst_baseline =
+          smoothing_ * dst_entropy + (1 - smoothing_) * ewma_dst_baseline;
+
+      ewmmd_src_baseline =
+          smoothing_ * std::fabs(src_entropy - ewma_src_baseline) +
+          (1 - smoothing_) * ewmmd_src_baseline;
+      ewmmd_dst_baseline =
+          smoothing_ * std::fabs(dst_entropy - ewma_dst_baseline) +
+          (1 - smoothing_) * ewmmd_dst_baseline;
+
+      threshold_src = ewma_src_baseline + sensitivity_ * ewmmd_src_baseline;
+      threshold_dst = ewma_dst_baseline - sensitivity_ * ewmmd_dst_baseline;
+  }
 };
 
 } // namespace Euclid
--- tools/experiment_runner/include/euclid/ExtendedCountSketch.hpp
+++ tools/experiment_runner/include/euclid/ExtendedCountSketch.hpp
@@ -1,20 +1,19 @@
 #ifndef EUCLID_EXTENDEDCOUNTSKETCH_HPP
 #define EUCLID_EXTENDEDCOUNTSKETCH_HPP
 
+#include <algorithm>
 #include <cstddef>
-#include <random>
 #include <cstdint>
 #include <functional>
+#include <iostream>
+#include <random>
 #include <vector>
-#include <algorithm>
 
 namespace Euclid {
 class ExtendedCountSketch {
 public:
   ExtendedCountSketch(uint64_t depth, uint64_t width)
-      : depth_(depth), width_(width), entropy_(), hash_functions_(),
-        table_(std::vector<std::vector<int64_t>>(
-            depth, std::vector<int64_t>(width, 0))) {
+      : depth_(depth), width_(width), entropy_(0), hash_functions_(), table_(depth, width) {
     hash_functions_.reserve(depth_);
     sign_functions_.reserve(depth_);
 
@@ -25,26 +24,29 @@ public:
       hash_functions_.push_back(create_hash_function(rng(), width_));
       sign_functions_.push_back(create_hash_function(rng(), 2));
     }
-
   }
 
   uint64_t get_depth() const { return depth_; }
 
   uint64_t get_width() const { return width_; }
 
+  uint64_t get_entropy() const { return entropy_; }
+
   void update(uint32_t address) {
-    const auto old_frequency{ estimate(address) };
+    const auto old_frequency{estimate(address)};
 
     for (size_t i = 0; i < depth_; ++i) {
-      const auto col{ hash_functions_[i](address) };
-      const auto sign{ sign_functions_[i](address) };
-      table_[i][col] += sign;
+      const auto col{hash_functions_[i](address)};
+      const auto sign{sign_functions_[i](address)};
+      table_(i, col) += sign;
     }
 
-    const auto new_frequency{ estimate(address) };
+    const auto new_frequency{estimate(address)};
 
-    const auto log_new_f{ new_frequency * std::log2(new_frequency) * (new_frequency > 0) };
-    const auto log_old_f{ old_frequency * std::log2(old_frequency) * (old_frequency > 0) };
+    const auto log_new_f{
+        (new_frequency > 0) ? new_frequency * std::log2(new_frequency) : 0};
+    const auto log_old_f{
+        (old_frequency > 0) ? old_frequency * std::log2(old_frequency) : 0};
 
     entropy_ += log_new_f - log_old_f;
   }
@@ -54,24 +56,52 @@ public:
     estimates.reserve(depth_);
 
     for (size_t i = 0; i < depth_; ++i) {
-        const int64_t col = hash_functions_[i](address);  // Find the column index for this row
-        const int64_t sign = 2 * sign_functions_[i](address) - 1;  // Determine the sign (+1 or -1)
-        estimates.push_back(table_[i][col] * sign);  // Get signed count
+      const int64_t col =
+          hash_functions_[i](address); // Find the column index for this row
+      const int64_t sign =
+          2 * sign_functions_[i](address) - 1; // Determine the sign (+1 or -1)
+      estimates.push_back(table_(i, col) * sign); // Get signed count
     }
 
     // Use std::nth_element to find the median
-    size_t mid = depth_ / 2;  // Index of the median
-    std::nth_element(estimates.begin(), estimates.begin() + mid, estimates.end());
-    return estimates[mid];  // Return the median
+    size_t mid = depth_ / 2; // Index of the median
+    std::nth_element(estimates.begin(), estimates.begin() + mid,
+                     estimates.end());
+
+    return estimates[mid]; // Return the median
+  }
+
+  void reset() {
+    table_.reset();
   }
 
 private:
+  class Matrix {
+  public:
+    Matrix(size_t rows, size_t cols)
+        : rows_(rows), cols_(cols), data_(std::vector<int64_t>(rows * cols, 0)) {}
+
+    inline int64_t &operator()(size_t row, size_t col) { return data_[row * cols_ + col]; }
+
+    inline const int64_t &operator()(size_t row, size_t col) const {
+      return data_[row * cols_ + col];
+    }
+
+    void reset() {
+      std::fill(data_.begin(), data_.end(), 0);
+    }
+
+  private:
+    std::vector<int64_t> data_;
+    size_t rows_, cols_;
+  };
+
   const uint64_t depth_;
   const uint64_t width_;
   double entropy_;
   std::vector<std::function<int64_t(uint32_t)>> hash_functions_;
   std::vector<std::function<int64_t(uint32_t)>> sign_functions_;
-  std::vector<std::vector<int64_t>> table_;
+  Matrix table_;
 
   // Helper function to create hash functions with different seeds
   std::function<int32_t(int32_t)> create_hash_function(int32_t seed,
